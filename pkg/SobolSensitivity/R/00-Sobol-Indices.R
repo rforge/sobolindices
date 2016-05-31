fde3828029094ec3c92c5e980d66a7fbb41d3d58
osu_mwang <osu_mwang@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-31 15:31:05 +0000

    Fix error in vector multiplication. (samplei[k,] term -> as.numeric applied)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sobolindices@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SobolSensitivity/R/00-Sobol-Indices.R
+++ pkg/SobolSensitivity/R/00-Sobol-Indices.R
@@ -293,7 +293,7 @@ LogitSIkintersample <- function(interaction, xdata, beta) {
     condexp <- rep(0, nrow(samplei))
     if (cond.sigma.i > 0) {
       for (k in 1:nrow(samplei)) {
-          mu.i <- cond.mu.i.b0 + t(cond.mu.i.b1) %*% samplei[k,]
+          mu.i <- cond.mu.i.b0 + t(cond.mu.i.b1) %*% as.numeric(samplei[k,])
 
           if (mu.i / cond.sigma.i > 0) {
             s.i <- 1 + mu.i / cond.sigma.i
@@ -314,7 +314,7 @@ LogitSIkintersample <- function(interaction, xdata, beta) {
       }  
     } else {
       for (k in 1:length(samplei)) {
-        mu.i <- cond.mu.i.b0 + t(cond.mu.i.b1) %*% samplei[k,] 
+        mu.i <- cond.mu.i.b0 + t(cond.mu.i.b1) %*% as.numeric(samplei[k,]) 
         condexp[k] <- exp(mu.i) / (1 + exp(mu.i))  
       }
     }
